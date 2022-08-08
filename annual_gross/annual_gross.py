00bfbab7d92cf0953313bd8f61bf2b6b363cb3db
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-08-08 17:06:18 +0200

    created classes

--- annual_gross/annual_gross.py
+++ annual_gross/annual_gross.py
@@ -5,18 +5,31 @@ import boto3
 month = 12
 sqs = boto3.client('sqs', region_name="us-east-1")
 queue_url = sqs.get_queue_url(QueueName='salary-calculator-orchestrator-queue').get('QueueUrl')
-
-
-def send_message_to_queue(self):
-    message = {"amount": self}
-    sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
+dynamodb = boto3.client('dynamodb')
+description = 'Annual gross'
+table_name = 'annual_gross'
+column = dynamodb.Table(table_name)
 
 
 def annual_gross_handler(event, context):
-    dynamodb = boto3.client('dynamodb')
-    annual_gross = event['amount'] * month
-    send_message_to_queue(annual_gross)
-    dynamodb.put_item(TableName='annual_gross',
-                      Item={'id': {'S': str(uuid.uuid4())}, 'amount': {'N': str(annual_gross)},
-                            'description': {'S': 'Annual gross'}})
-    return {'Annual gross': annual_gross}
+    annual_gross = AnnualGross.calculate_annual_gross(event['amount'])
+    Sqs.send_message_to_queue(annual_gross)
+    DynamoDb.save_data(annual_gross)
+    return {description: annual_gross}
+
+
+class AnnualGross:
+    def calculate_annual_gross(self):
+        return self * month
+
+
+class DynamoDb:
+    def save_data(self):
+        dynamodb.put_item(TableName=table_name,
+                          Item={'id': {'S': str(uuid.uuid4())}, 'amount': {'N': str(self)},
+                                'description': {'S': description}})
+
+class Sqs:
+    def send_message_to_queue(self):
+        message = {"amount": self}
+        sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
